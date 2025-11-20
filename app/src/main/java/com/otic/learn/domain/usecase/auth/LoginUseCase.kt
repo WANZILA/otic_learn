52cef99770856477a3c50fa5a5351372f5917631
Wanzila <wanzilro@gmail.com>
2025-11-20 12:34:37 +0300

    feat: add core use cases

--- app/src/main/java/com/otic/learn/domain/usecase/auth/LoginUseCase.kt
+++ app/src/main/java/com/otic/learn/domain/usecase/auth/LoginUseCase.kt
@@ -1,4 +1,11 @@
 package com.otic.learn.domain.usecase.auth
 
-class LoginUseCase {
+import com.otic.learn.domain.repo.AuthRepository
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val repo: AuthRepository
+) {
+    suspend operator fun invoke(email: String, password: String) =
+        repo.login(email, password)
 }
